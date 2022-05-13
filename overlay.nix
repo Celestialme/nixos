ee1aaadac985e7c757837e7bc9819a818bb0b216
Celestialme <Celestialme@users.noreply.github.com>
2022-05-13 14:35:42 +0400

    Update overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -18,5 +18,13 @@ self: super: {
       --add-flags "--no-sandbox"
   '';
 });
+
+
+   nvidia_340_5_16 = super.linuxKernel.packages.linux_5_16.nvidia_x11_legacy340.overrideAttrs (oldAttrs: {
+           patches = [ ./0001_kernel_5.7.patch  ./0003_kernel_5.9.patch   ./0005_kernel_5.11.patch  ./0007_kernel_5.15.patch ./0002_kernel_5.8.patch  ./0004_kernel_5.10.patch  ./0006_kernel_5.14.patch  ./0008_kernel_5.16.patch ];
+           broken = false; 
+
+});
+
 }
 

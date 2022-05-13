62477ab0d2ec7ef1c8368ea4641139fc7e8dfe13
Celestialme <Celestialme@users.noreply.github.com>
2022-05-13 14:49:36 +0400

    Update overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -19,12 +19,14 @@ self: super: {
   '';
 });
 
-
-   nvidia_340_5_16 = super.linuxKernel.packages.linux_5_16.nvidia_x11_legacy340.overrideAttrs (oldAttrs: {
-           patches = [ ./0001_kernel_5.7.patch  ./0003_kernel_5.9.patch   ./0005_kernel_5.11.patch  ./0007_kernel_5.15.patch ./0002_kernel_5.8.patch  ./0004_kernel_5.10.patch  ./0006_kernel_5.14.patch  ./0008_kernel_5.16.patch ];
-           broken = false; 
+ nvidia_340_5_17 = super.linuxKernel.packages.linux_5_17.nvidia_x11_legacy340.overrideAttrs (oldAttrs: {
+   patches = [ ./0001_kernel_5.7.patch ./0002_kernel_5.8.patch ./0003_kernel_5.9.patch ./0004_kernel_5.10.patch ./0005_kernel_5.11.patch ./0006_kernel_5.14.patch ./0007_kernel_5.15.patch ./0008_kernel_5.16.patch ./0009-kernel-5.17.patch];
+   broken = false; 
 
 });
+});
+
+
 
 }
 
